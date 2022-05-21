fa06738409945e24bb6f2ab567a45f3e15c65473
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-20 20:53:41 -0500

    made responsive * styles for mobile view

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -4,12 +4,9 @@ import ProductCard from './ProductCard';
 const ProductList = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.description}>
+      <h2 className={styles.description}>
         A delicate mix of Asian, Nomadic, and Oriental kitchen in one place.
-        <br />
-        Explore a variety of delicious plates.
-        <br /> Experience the Turkmen hospitality.
-      </p>
+      </h2>
       <div className={styles.wrapper}>
         <ProductCard />
         <ProductCard />
