2537f98db649b7f8c20a87ac3decdebfbfc08dd8
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-21 23:34:19 -0500

    created cart slice

---
+++ redux/cart.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        products: [],
+        totalPrice: 0
+    },
+    reducers: {
+        addProduct: (state, action) => {
+            state.products.push(action.payload);
+            state.totalPrice += action.payload.price * action.payload.qty;
+        },
+        resetCart: (state, action) => {
+            state = initialState;
+        }
+    }
+});
+
+// Extract the action creators object and the reducer
+const { actions, reducer} = cartSlice;
+
+// Extract and export each action creator by name
+export const {addProduct, resetCart} = actions;
+
+// Export the reducer, either as a default or named export
+export default reducer;
